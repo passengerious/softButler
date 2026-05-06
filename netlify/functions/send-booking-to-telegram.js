dd95dd6874c556474e4835e7df145e3d1bd76569
podunai <podunaiandrii@gmail.com>
2026-05-06 10:24:06 +0300

    changed Id group

--- netlify/functions/send-booking-to-telegram.js
+++ netlify/functions/send-booking-to-telegram.js
@@ -1,5 +1,5 @@
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const GROUP_ID = process.env.TELEGRAM_GROUP_ID || '-1003922476267';
+const GROUP_ID = process.env.TELEGRAM_GROUP_ID || '-1003502873196';
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 const TARGET_CHAT_ID = GROUP_ID || CHAT_ID;
 
